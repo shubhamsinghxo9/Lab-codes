8818a3da003cedde0f02b5b0ce98cdb12390bb90
Shubham Singh <shubhamsingh852877@gmail.com>
2025-09-16 11:23:32 +0530

    Array.java

---
+++ src/shubhamsingh12/Array.java
@@ -0,0 +1,13 @@
+package xyz;
+
+public class Array {
+	public static void main(String[] args) {
+		int marks[]= {90,85,70,95,100};
+		
+		System.out.println("Marks are:");
+		for(int i=0;i<marks.length;i++) {
+			System.out.println(marks[i]);
+		}
+	}
+
+}
