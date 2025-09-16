160250ab0335c1b6c4393528ced6fce2a0b26635
Shubham Singh <shubhamsingh852877@gmail.com>
2025-09-16 11:24:39 +0530

    Create Condition.java

---
+++ src/shubhamsingh12/Condition.java
@@ -0,0 +1,12 @@
+package shubhamsingh12;
+
+public class Condition {
+	public static void main (String args[]) {
+		int sum = 0;
+		
+		if(sum>30)
+			System.out.println("Success");
+		else
+			System.out.println("Fail");
+	}
+}
